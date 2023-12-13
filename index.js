3e28d1e15535a310a14fcc0bcf6965014afc8e67
anurag270102 <anuragdalsaniya@gmail.com>
2023-12-13 11:04:47 +0530

    add DRUM kit

---
+++ index.js
@@ -0,0 +1,54 @@
+var numberofbutton=document.querySelectorAll(".drum").length;
+for (var i = 0; i<numberofbutton ; i++) 
+    {
+        document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+            var buttoninnerhtml=this.innerHTML;
+            makesound(buttoninnerhtml);
+            buttonanimation(buttoninnerhtml);
+        });
+    }
+    addEventListener("keydown",function(event){
+        makesound(event.key);
+        buttonanimation(event.key);
+    })
+    function makesound(key){
+        switch (key) {
+            case "w":
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+             case "s":
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            default:
+                console.log(buttoninnerhtml);
+         }
+    }
+function buttonanimation(curentkey){
+     var activebutton=document.querySelector("."+curentkey);
+     activebutton.classList.add("pressed");
+     setTimeout(function(){
+      activebutton.classList.remove("pressed");
+     },100);
+}
